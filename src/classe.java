1e1566f7e97ca098ca9adee18e30313c2b3f573f
MPNuri <mpnurullah02@gmail.com>
2022-07-09 01:02:52 -0400

    added second println

---
+++ src/classe.java
@@ -0,0 +1,9 @@
+public class classe {
+    public static void main(String[] args) {
+
+        System.out.println("so cooll");
+
+        System.out.println("changed class turns blue");
+
+    }
+}
---
+++ src/x.java
@@ -0,0 +1,52 @@
+public class x {
+    public static void main(String[] args) {
+
+        System.out.println("murillo says helioo");
+
+        //Git -> commit -> Commit and push -> Push u can do that
+        // lets see how we can send a replaced code into github
+
+        /**
+         * 1- working directory = current working area, it is where files live
+
+         * 2- index directory (staging area) = after .add comment
+           2.1- after creating another class java will ask us to add file to GIT. 4
+             - if you add it will be on index directory, if you dont it will be on working directory
+               - added classes will be green, not-added classes will contain red color
+
+         * 3- we can add a non-added class inside GitHub later on by right click onto class (CLICK GIT AND ADD)
+
+         * 4- When class is blue that means something changed inside the class.
+
+         * 5- when we have directory GIT cannot track the class (code) but with index GIT can track the code
+
+         * 6- You can roll back the indexed classes to be untracked
+
+         */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
